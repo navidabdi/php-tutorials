1c7eb0c9d153f68f22e362c6191bfe2239586906
navidabdi <artsanaat@gmail.com>
2022-09-05 14:44:29 +0300

    String Functions

---
+++ 08_string_functions.php
@@ -0,0 +1,35 @@
+<?php
+/* ---------- String Functions -------- */
+
+/*
+  Functions to work with strings
+  https://www.php.net/manual/en/ref.strings.php
+*/
+
+$string = 'Hello World';
+
+// Get the length of a string
+// echo strlen($string);
+
+// Find the position of the first occurrence of a substring in a string
+// echo strpos($string, 'l');
+
+// Find the position of the last occurrence of a substring in a string
+// echo strrpos($string, 'o');
+
+// Reverse a string
+// echo strrev($string);
+
+// Convert all characters to lowercase
+// echo strtolower($string);
+
+// Convert all characters to uppercase
+// echo strtoupper($string);
+
+// Uppercase the first character of each word
+// echo ucwords($string);s
+
+// String replace
+// echo str_replace('World', 'Everyone', $string);
+
+echo str_repeat("-=", 10);
